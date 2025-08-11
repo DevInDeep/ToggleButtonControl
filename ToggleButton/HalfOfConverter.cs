09acbd03d8e146c6f08c047cf58fe174c115924f
vpavlevski <vpavlevski@yahoo.com>
2025-08-11 20:21:17 +0200

    Add project files.

---
+++ ToggleButton/HalfOfConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace ToggleButton
+{
+    internal class HalfOfConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double currentValue = (double)value;
+            return currentValue / 2;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double currentValue = (double)value;
+            return currentValue * 2;
+        }
+    }
+}
