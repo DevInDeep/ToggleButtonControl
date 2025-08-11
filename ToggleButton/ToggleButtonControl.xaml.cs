ea9cb02b6f164ccdef30d3a4ab411c2c957331c3
vpavlevski <vpavlevski@yahoo.com>
2025-08-11 20:58:40 +0200

    Add event

--- ToggleButton/ToggleButtonControl.xaml.cs
+++ ToggleButton/ToggleButtonControl.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System.Windows.Input;
-using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Controls;
 
 namespace ToggleButton
 {
@@ -9,11 +9,13 @@ namespace ToggleButton
     /// </summary>
     public partial class ToggleButtonControl : UserControl
     {
+        public event EventHandler<bool> ToggleButtonStateChanged;
         public ToggleButtonControl() => InitializeComponent();
         private void ToggleButton_Click(object sender, MouseButtonEventArgs e)
         {
             IsChecked = !IsChecked;
             background.Background = IsChecked ? new SolidColorBrush(OnColor) : new SolidColorBrush(Colors.Gray);
+            ToggleButtonStateChanged?.Invoke(this, IsChecked);
         }
     }
 }
